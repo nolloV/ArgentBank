ffe8245574da125b53db38a27d228349f29afcee
nolloV <142914191+nolloV@users.noreply.github.com>
2024-01-25 11:28:53 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import Users from "./pages/Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users" element={<Users />} />
+          {/* <Route path="/Host/:id" element={<Host />} /> */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </div>
+      <Footer></Footer>
+    </Router>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import "./footer.scss";
+import React from "react";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <p class="footer__text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+}
+
+export default Footer;
